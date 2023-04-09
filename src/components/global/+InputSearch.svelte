de0dd52a6179b98f08cb39c595fc5f816cbb403a
agustin <agustin.ryo@gmail.com>
2023-04-10 01:45:46 +0200

    refactor: menu: organization menu items

---
+++ src/components/global/+InputSearch.svelte
@@ -0,0 +1,15 @@
+<div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden transition duration-500">
+    <div class="grid place-items-center h-full w-12 text-gray-300">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+        </svg>
+    </div>
+
+    <input
+        class="peer h-full w-full outline-none text-sm text-gray-700 pr-2 border-none"
+        type="text"
+        id="search"
+        placeholder="Search..."
+        autocomplete="off"
+    />
+</div>
--- src/components/navigation/+Menu.svelte
+++ src/components/navigation/+Menu.svelte
@@ -1,33 +1,7 @@
 <script lang="ts">
-	import { GlobalPage, currentPage } from '$stores/GlobalStore';
+	import InputSearch from '$components/global/+InputSearch.svelte';
+    import { GlobalPage, currentPage } from '$stores/GlobalStore';
     import { Headphones, Menu } from 'lucide-svelte';
-
-const navbar = [
-    {
-        name: "Home",
-        href: "/",
-    },
-    {
-        name: "Directory",
-        href: "/directory",
-    },
-    {
-        name: "Album",
-        href: "/album",
-    },
-    {
-        name: "Artist",
-        href: "/artist",
-    },
-    {
-        name: "Docs",
-        href: "/docs",
-    },
-    {
-        name: "Settings",
-        href: "/settings",
-    },
-];
 
     let _currentPage_: GlobalPage;
 
@@ -73,28 +47,14 @@ const mobileIcons = "dark:text-zinc-300 text-black hover:text-zinc-500 dark:hove
 
     {#each navbar_ as { name, view }}
         <nav class="pt-6 flex flex-row space-x-6">
-            <div on:click={()=>{changePage(view)}} class="dark:text-zinc-300 text-black hover:text-zinc-500 dark:hover:text-zinc-400 hidden sm:block">
+            <div on:click={()=>{changePage(view)}} class="dark:text-zinc-300 text-black hover:text-zinc-500 dark:hover:text-zinc-400 hidden sm:block cursor-pointer">
                 {name}
             </div>
         </nav>
     {/each}
 
     <div class="flex items-center justify-center">
-        <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden transition duration-500">
-            <div class="grid place-items-center h-full w-12 text-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                </svg>
-            </div>
-
-            <input
-                class="peer h-full w-full outline-none text-sm text-gray-700 pr-2 border-none"
-                type="text"
-                id="search"
-                placeholder="Search..."
-                autocomplete="off"
-            />
-        </div>
+        <InputSearch />
     </div>
 
     <button class="nav-toggle mx-8 my-5 sm:invisible display-none sm:hidden" aria-label="menu button">
@@ -103,11 +63,11 @@ const mobileIcons = "dark:text-zinc-300 text-black hover:text-zinc-500 dark:hove
 </header>
 
 <header class="nav-menu hidden bg-white dark:bg-zinc-900">
-    {#each navbar as { name, href }}
+    {#each navbar_ as { name, view }}
         <nav class="m-6 flex flex-col space-y-3">
-            <a href={href} class="dark:text-zinc-300 text-black hover:text-zinc-500 dark:hover:text-zinc-400 sm:hidden">
+            <div on:click={()=>{changePage(view)}} class="dark:text-zinc-300 text-black hover:text-zinc-500 dark:hover:text-zinc-400 sm:hidden cursor-pointer">
                 {name}
-            </a>
+            </div>
         </nav>
     {/each}
 </header>
