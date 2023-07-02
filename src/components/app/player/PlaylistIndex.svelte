5e176ea309a4af3fabfa344556d3fceeb79fa04a
agustin <agustin.ryo@gmail.com>
2023-07-02 04:46:04 +0200

    feat: playlist: Pantalla de información de la canción actual

--- src/components/app/player/PlaylistIndex.svelte
+++ src/components/app/player/PlaylistIndex.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Child } from "$models/servers/subsonic";
     import PlaylistStore from "$stores/PlaylistStore";
+	import PlayerInformationSong from "./partials/PlayerInformationSong.svelte";
 	import PlayerLineMusic from "./partials/PlayerLineMusic.svelte";
 
     let localPlaylist: Child[] = [];
@@ -12,6 +13,7 @@
 </script>
 
 <div class="px-3 mx-3 w-4/12 divide-y">
+    <PlayerInformationSong />
     {#each localPlaylist as song, index}
         {#if !song.isDir}
             <PlayerLineMusic bind:song={song} index={index} />
---
+++ src/components/app/player/partials/PlayerInformationSong.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+	import { SubsonicAPI, type Child } from "$models/servers/subsonic";
+	import { ServerConfigPersistent } from "$stores/ServerConfigStore";
+    import { currentSong } from "$stores/CurrentPlaySong";
+    
+    export let songId = "-1";
+
+    // Si hay un error al cargar la imagen, se intenta cargar la imagen del padre
+    // Si hay un error al cargar la imagen del padre, se carga una imagen por defecto
+    let countHandleError = 0;
+    let imageUrl = "https://placehold.it/210x310";
+    let imageUrlParent = "https://placehold.it/210x310";
+    let fallback = "https://placehold.it/210x310";
+
+    const handleError = (ev: { target: { src: string; } } | any) => {
+        countHandleError++;
+        if (countHandleError > 1) {
+            ev.target.src = fallback;
+            return;
+        }
+        ev.target.src = imageUrlParent;
+        return;
+    };
+
+    /**
+     * Canción actual
+     */
+    let song: Child|undefined;
+
+    /**
+     * API de Subsonic
+     */
+    let api: SubsonicAPI = initSubsonicApi();
+
+    /**
+     * Inicializa la API de Subsonic
+     * 
+     * @returns {SubsonicAPI}
+     */
+    function initSubsonicApi(): SubsonicAPI {
+
+        // Obtenemos los datos del servidor desde la memoria persistente
+        let server = ServerConfigPersistent.get();
+        console.log(server);
+
+        const api = new SubsonicAPI({
+            url: server.serverUrl,
+            type: server.serverType, // or "generic" or "navidrome"
+        });
+
+        api.loginSync({
+            username: server.username,
+            password: server.password,
+            serverName: server.serverName,
+            version: server.serverVersion,
+        });
+
+        return api;
+    }
+
+    /**
+     * Obtiene la información de la canción actual
+     */
+    currentSong.subscribe((currentSong) => {
+        if (currentSong && currentSong.songId !== "-1") {
+            songId = currentSong.id;
+            song = currentSong as Child;
+        }
+    });
+
+    /**
+     * Obtiene la imagen de la canción actual
+     */
+    $: if (songId && songId !== "-1") {
+        countHandleError = 0;
+        imageUrl = api.getCoverArtWoFetchSync({id: songId});
+        if (song !== undefined) {
+            imageUrlParent = api.getCoverArtWoFetchSync({id: song.parent!});
+        }
+    }
+
+    /**
+     * Devuelve la duración en formato mm:ss
+     */
+    function getDurationHuman(duration: number|undefined) {
+
+        // Si no hay duración, se devuelve 00:00
+        if (duration === undefined) return "00:00";
+
+        // Si hay duración, se devuelve en formato mm:ss
+        let minutes = Math.floor(duration / 60);
+        let seconds = duration - minutes * 60;
+
+        let mmStr = minutes.toString().padStart(2, "0");
+        let ssStr = seconds.toString().padStart(2, "0");
+        return `${mmStr}:${ssStr}`;
+    }
+
+</script>
+
+<div class="divider-y w-full my-3">
+    <div class="flex flex-row">
+        <img loading="lazy" src={imageUrl} on:error={handleError} data-amplitude-song-info="cover_art_url" class="w-24 h-24 rounded-sm mr-3 border-bg-player-light-background dark:border-cover-dark-border object-cover" alt={song?.title}/>
+        <div class="flex flex-col">
+            <div class="text-sm font-semibold text-bg-player-light-text dark:text-cover-dark-text">{song?.title ?? '--'}</div>
+            <div class="text-xs text-bg-player-light-text dark:text-cover-dark-text">{song?.artist ?? '--'}</div>
+            <div class="text-xs text-bg-player-light-text dark:text-cover-dark-text">{song?.album ?? '--'}</div>
+            <div class="text-xs text-bg-player-light-text dark:text-cover-dark-text">{getDurationHuman(song?.duration)}</div>
+        </div>
+    </div>
+</div>
