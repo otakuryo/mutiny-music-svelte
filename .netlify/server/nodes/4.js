8f8e9b6af81c40609b98a4e2bd623152a253c077
agustin <agustin.ryo@gmail.com>
2023-07-08 12:08:01 +0200

    build: netflify: Configuration and compilation

---
+++ .netlify/server/nodes/4.js
@@ -0,0 +1,8 @@
+
+
+export const index = 4;
+export const component = async () => (await import('../entries/pages/settings/_page.svelte.js')).default;
+export const file = '_app/immutable/components/pages/settings/_page.svelte-22f3b52e.js';
+export const imports = ["_app/immutable/components/pages/settings/_page.svelte-22f3b52e.js","_app/immutable/chunks/index-6a905201.js","_app/immutable/chunks/index-3d909c6e.js","_app/immutable/chunks/stores-3e29ef80.js","_app/immutable/chunks/singletons-4a06a8de.js","_app/immutable/chunks/index-bf44796e.js"];
+export const stylesheets = ["_app/immutable/assets/index-21192275.css"];
+export const fonts = [];
