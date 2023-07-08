6bb00c45d0660d6cf21b91c556324ab749632e8d
agustin <agustin.ryo@gmail.com>
2023-07-08 12:08:01 +0200

    build: netflify: Configuration and compilation

--- .netlify/server/nodes/4.js
+++ .netlify/server/nodes/4.js
@@ -2,7 +2,13 @@
 
 export const index = 4;
 export const component = async () => (await import('../entries/pages/settings/_page.svelte.js')).default;
+<<<<<<< HEAD
 export const file = '_app/immutable/components/pages/settings/_page.svelte-5ecb129d.js';
 export const imports = ["_app/immutable/components/pages/settings/_page.svelte-5ecb129d.js","_app/immutable/chunks/index-6a905201.js","_app/immutable/chunks/index-99274c57.js","_app/immutable/chunks/stores-3e29ef80.js","_app/immutable/chunks/singletons-4a06a8de.js","_app/immutable/chunks/index-000176f9.js"];
 export const stylesheets = ["_app/immutable/assets/index-5330fd26.css"];
+=======
+export const file = '_app/immutable/components/pages/settings/_page.svelte-22f3b52e.js';
+export const imports = ["_app/immutable/components/pages/settings/_page.svelte-22f3b52e.js","_app/immutable/chunks/index-6a905201.js","_app/immutable/chunks/index-3d909c6e.js","_app/immutable/chunks/stores-3e29ef80.js","_app/immutable/chunks/singletons-4a06a8de.js","_app/immutable/chunks/index-bf44796e.js"];
+export const stylesheets = ["_app/immutable/assets/index-21192275.css"];
+>>>>>>> 38f96ad (build: netflify: Configuration and compilation)
 export const fonts = [];
