d09cd131a0de0e377a1572192e23273ac97d2a39
agustin <agustin.ryo@gmail.com>
2023-07-17 01:27:22 +0200

    feat: directory: Add Starred function

--- src/components/app/directory/partials/DirectoryLineMusic.svelte
+++ src/components/app/directory/partials/DirectoryLineMusic.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { SubsonicAPI } from "$models/servers/subsonic";
 	import type { Child } from "$models/servers/subsonic/types";
-	import { Play, Pause, ListEnd, PlusCircle, CheckCircle } from "lucide-svelte";
+	import { Play, Pause, ListEnd, PlusCircle, CheckCircle, Star } from "lucide-svelte";
     import { isPlaying } from "$stores/PlayerStore";
 	import PlayerStore from "$stores/PlayerStore";
 	import PlaylistStore from '$stores/PlaylistStore';
@@ -66,6 +66,21 @@
         }
     }
 
+    /**
+     * Toggle star
+     * 
+     * If star is true, then the song is starred, so we want to remove it from starred songs
+     */
+    async function toggleStar() {
+        if (song.starred === undefined) {
+            await api.star({id: song.id});
+            song.starred = new Date();
+        } else {
+            await api.unstar({id: song.id});
+            song.starred = undefined;
+        }
+    }
+
     function getDurationHuman() {
 
         // Si no hay duración, se devuelve 00:00
@@ -102,6 +117,15 @@
             </div>
 
             <div class="ml-auto flex flex-row">
+
+                <!-- Añadir a favoritos -->
+                <div on:click={toggleStar} on:keydown={toggleStar}>
+                    {#if song.starred !== undefined }
+                        <Star class="stroke-current text-slate-900 dark:text-yellow-200 h-6 w-12" fill="yellow"/>
+                    {:else}
+                        <Star class="stroke-current text-slate-900 dark:text-yellow-200 h-6 w-12" />
+                    {/if}
+                </div>
 
                 <!-- Añadir a la playlist -->
                 <div on:click={addSongToPlaylist} on:keypress={addSongToPlaylist}>
