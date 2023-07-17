7e778634115d528cc0d29d80e1f89cd7ca056978
agustin <agustin.ryo@gmail.com>
2023-07-18 00:10:57 +0200

    build: netlify: clear build folder

--- .netlify/server/nodes/3.js
+++
@@ -1,9 +0,0 @@
-import * as universal from '../entries/pages/directory/_page.js';
-
-export const index = 3;
-export const component = async () => (await import('../entries/pages/directory/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/directory/_page.svelte-fe0f9ac5.js';
-export { universal };
-export const imports = ["_app/immutable/components/pages/directory/_page.svelte-fe0f9ac5.js","_app/immutable/chunks/index-6a905201.js","_app/immutable/chunks/index-000176f9.js","_app/immutable/chunks/index-99274c57.js","_app/immutable/chunks/stores-3e29ef80.js","_app/immutable/chunks/singletons-4a06a8de.js","_app/immutable/modules/pages/directory/_page.js-7dc16dc1.js","_app/immutable/chunks/_page-4039aba4.js"];
-export const stylesheets = ["_app/immutable/assets/_page-856c326c.css","_app/immutable/assets/index-5330fd26.css"];
-export const fonts = [];
