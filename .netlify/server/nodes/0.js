f94e53ddf8fbe3c4690c98634212cfd3d028386b
agustin <agustin.ryo@gmail.com>
2023-07-18 00:19:37 +0200

    build: netlify: clear build folder

--- .netlify/server/nodes/0.js
+++
@@ -1,9 +0,0 @@
-import * as universal from '../entries/pages/_layout.js';
-
-export const index = 0;
-export const component = async () => (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const file = '_app/immutable/components/layout.svelte-ccbc0076.js';
-export { universal };
-export const imports = ["_app/immutable/components/layout.svelte-ccbc0076.js","_app/immutable/chunks/index-6a905201.js","_app/immutable/modules/pages/_layout.js-e235ae78.js","_app/immutable/chunks/_layout-9f4d2fb2.js"];
-export const stylesheets = [];
-export const fonts = [];
