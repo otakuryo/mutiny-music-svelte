c08eee60ef5d3e3c6acd3631405a4243f17234e7
agustin <agustin.ryo@gmail.com>
2023-07-30 12:41:14 +0200

    refactor: musicfolders: Update API and retrive data

--- src/components/app/musicFolder/MusicFolderIndex.svelte
+++ src/components/app/musicFolder/MusicFolderIndex.svelte
@@ -1,49 +1,16 @@
 
 <script lang="ts">
-    import { type MusicFolders, SubsonicAPI, type SubsonicBaseResponse } from '$models/servers/subsonic';
-    import { ServerConfigPersistent } from '$stores/ServerConfigStore';
-	// import DirectoryIndex from '../directory/DirectoryIndex.svelte';
+	import { initSubsonicApi } from '$lib/js/Helpers';
+    import type { MusicFolders, SubsonicAPI, SubsonicBaseResponse } from '$models/servers/subsonic';
 	import MusicFolderLine from './partials/MusicFolderLine.svelte';
 
-    let _data_ = [];
+    type MusicFoldersType = (SubsonicBaseResponse & { musicFolders: MusicFolders });
     let api: SubsonicAPI;
 
-    async function initSubsonicApi() {
-
-        // Obtenemos los datos del servidor desde la memoria persistente
-        let server = ServerConfigPersistent.get();
-        
-        const api = new SubsonicAPI({
-            url: server.serverUrl,
-            type: server.serverType, // or "generic" or "navidrome"
-        });
-
-        api.loginSync({
-            username: server.username,
-            password: server.password,
-            serverName: server.serverName,
-            version: server.serverVersion,
-        });
-
-        return api;
-    }
-
-    async function getDataFromServer() {
+    async function getDataFromServer(): Promise<MusicFoldersType> {
         api = await initSubsonicApi();
 
-        const resPing: SubsonicBaseResponse = await api.ping()
-
-        // Si no hay una respuesta por parte del servidor, se para y no se hace nada
-        if (resPing.status !== "ok") {
-            return [];
-        }
-        const resMusicFolders: SubsonicBaseResponse & { musicFolders: MusicFolders }  = await api.getMusicFolders()
-
-        if (!resMusicFolders || !resMusicFolders.musicFolders || !resMusicFolders.musicFolders.musicFolder) {
-            return [];
-        }
-        
-        console.log(resPing,resMusicFolders, resMusicFolders.musicFolders.musicFolder);
+        const resMusicFolders: MusicFoldersType  = await api.getMusicFolders()
         
         return resMusicFolders;
 
@@ -55,8 +22,10 @@
     {#await getDataFromServer()}
         <div>loading...</div>
     {:then musicFolders}
-        {#each musicFolders.musicFolders.musicFolder as musicFolder}
-            <MusicFolderLine folder={musicFolder} />
-        {/each}
+        {#if musicFolders && musicFolders.musicFolders && musicFolders.musicFolders.musicFolder && musicFolders.musicFolders.musicFolder.length > 0}
+            {#each musicFolders.musicFolders.musicFolder as musicFolder}
+                <MusicFolderLine folder={musicFolder} />
+            {/each}
+        {/if}
     {/await}
 </div>
