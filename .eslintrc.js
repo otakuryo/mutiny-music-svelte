81a34910ad68cd6281e7b45690b97895f3849e4a
agustin <agustin.ryo@gmail.com>
2023-07-30 12:19:29 +0200

    update: eslint: Update eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
+	extends: [
+		'eslint:recommended',
+		'prettier',
+		'plugin:svelte/recommended'
+	],
+	rules: {
+
 	},
 	env: {
 		browser: true,
