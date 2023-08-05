267dc9c348d35504aed729c97cab37353a7949a3
agustin <agustin.ryo@gmail.com>
2023-08-05 21:25:03 +0200

    style: menu: Adjust spaces and disposition

--- src/components/navigation/+MenuRoute.svelte
+++ src/components/navigation/+MenuRoute.svelte
@@ -44,17 +44,17 @@
 </script>
 
 <!-- desktop menu -->
-<header class=" fixed h-20 w-screen dark:text-zinc-300 bg-white dark:bg-zinc-900 z-50">
-    <div class="text-lg mx-auto max-w-6xl flex flex-row justify-between  ">
-        <a href="/" class="mx-8 my-5 flex items-center " aria-label="Website Logo">
+<header class=" fixed h-16 w-screen dark:text-zinc-300 bg-white dark:bg-zinc-900 z-50 dark:border-b-white border-b-2 border-b-zinc-300">
+    <div class="text-lg mx-auto max-w-6xl flex flex-row justify-between h-full">
+        <a href="/" class="mx-6 flex items-center " aria-label="Website Logo">
             <Headphones class="w-8 h-8"/>
             <span class="self-center sm:hidden text-2xl whitespace-nowrap dark:text-white ml-3 mt-1">
-                NoFuss
+                 Mutiny
             </span>
         </a>
     
         {#each navbar_ as { name, href }}
-            <nav class="pt-6 flex flex-row space-x-6">
+            <nav class="flex flex-row space-x-3 h-full items-center">
                 <a href={href} class="dark:text-zinc-300 text-black hover:text-zinc-500 dark:hover:text-zinc-400 hidden sm:block cursor-pointer">
                     {name}
                 </a>
@@ -65,21 +65,23 @@
             <InputSearch />
         </div> -->
     
-        <button class="nav-toggle mx-8 my-5 sm:invisible display-none sm:hidden" aria-label="menu button" on:keypress={toggleMenu} on:click={toggleMenu}>
+        <button class="nav-toggle mx-6 my-5 sm:invisible display-none sm:hidden" aria-label="menu button" on:keypress={toggleMenu} on:click={toggleMenu}>
             <Menu class="w-8 h-8" />
         </button>
     </div>
 </header>
 
 <!-- Space for the menu -->
-<div class="h-20 w-screen"></div>
+<div class="h-16 w-screen"></div>
 
-<header class="nav-menu hidden bg-white dark:bg-zinc-900">
-    {#each navbar_ as { name, href }}
-        <nav class="m-6 flex flex-col space-y-3">
-            <a href={href} class="dark:text-zinc-300 text-black hover:text-zinc-500 dark:hover:text-zinc-400 sm:hidden cursor-pointer">
-                {name}
-            </a>
-        </nav>
-    {/each}
+<header class="nav-menu hidden z-50 main-color relative h-screen">
+    <div class="h-full overflow-scroll">
+        {#each navbar_ as { name, href }}
+            <nav class="m-6 flex flex-col space-y-3">
+                <a href={href} class="dark:text-zinc-300 text-black hover:text-zinc-500 dark:hover:text-zinc-400 sm:hidden cursor-pointer">
+                    {name}
+                </a>
+            </nav>
+        {/each}
+    </div>
 </header>
