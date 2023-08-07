0ea503d2a6d7350581df62ddebc2745cc753e4b6
agustin <agustin.ryo@gmail.com>
2023-08-08 00:34:10 +0200

    refactor: artist: update refresh view and create better response type

--- src/components/app/artists/ArtistIndex.svelte
+++ src/components/app/artists/ArtistIndex.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import { SubsonicAPI, type SubsonicBaseResponse, type ArtistsID3, type ArtistWithAlbumsID3 } from '$models/servers/subsonic';
-    import { ServerConfigPersistent } from '$stores/ServerConfigStore';
+    import type { SubsonicAPI, SubsonicBaseResponse, ArtistWithAlbumsID3 } from '$models/servers/subsonic';
 	import { onMount } from 'svelte';
 	import LineBack from '$components/global/Navigation/LineBack.svelte';
 	import LineAlbumId3 from '$components/global/Navigation/LineAlbumID3.svelte';
+	import { MainServerSubsonicAPI } from '$lib/js/Helpers';
 
     type IndexesTypeLocal = (SubsonicBaseResponse & { artist: ArtistWithAlbumsID3 });
 
@@ -16,33 +16,16 @@
         dataFromServer = getDataFromServer();
     });
 
-    async function initSubsonicApi() {
-
-        // Obtenemos los datos del servidor desde la memoria persistente
-        let server = ServerConfigPersistent.get();
-        console.log(server);
-        
-        const api = new SubsonicAPI({
-            url: server.serverUrl,
-            type: server.serverType, // or "generic" or "navidrome"
-        });
-
-        api.loginSync({
-            username: server.username,
-            password: server.password,
-            serverName: server.serverName,
-            version: server.serverVersion,
-        });
-
-        return api;
+    $: if(artistID) {
+        refreshViewOnClick();
     }
 
     async function getDataFromServer(): Promise<IndexesTypeLocal> {
 
         try {
-            api = await initSubsonicApi();
+            api = MainServerSubsonicAPI();
 
-            let resMusic: IndexesTypeLocal = await api.getArtist({id: artistID});
+            let resMusic: IndexesTypeLocal = await api.getArtistWithAlbums({id: artistID});
             return resMusic;
 
         } catch (error) {
@@ -65,12 +48,12 @@
         {#if serverResponse.artist.album && serverResponse.artist.album.length > 0}
         
             <div class="navigation-sticky">
-                <LineBack url="/artists" name="Volver" refreshViewOnClick={refreshViewOnClick} />
+                <LineBack url="/artists" name="Volver" refreshViewOnClick={()=>{}} />
             </div>
             
             {#each serverResponse.artist.album as album}
 
-                <LineAlbumId3 album={album} api={api} refreshViewOnClick={refreshViewOnClick} />
+                <LineAlbumId3 album={album} api={api} refreshViewOnClick={()=>{}} />
                     
             {/each}
 
