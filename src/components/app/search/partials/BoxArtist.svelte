76de25890a9cfd23fa80ab0e9c20b89e5157e740
agustin <agustin.ryo@gmail.com>
2023-08-08 02:16:06 +0200

    refactor: search: Update image cover and size of layout

--- src/components/app/search/partials/BoxArtist.svelte
+++ src/components/app/search/partials/BoxArtist.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
+	import ImgCover from "$components/global/ImgCover.svelte";
 	import type { SubsonicAPI } from "$models/servers/subsonic";
 	import type { ArtistID3 } from "$models/servers/subsonic/types";
-	import ImgCover from "./ImgCover.svelte";
-	import { getDurationHuman } from "$lib/js/Helpers";
-
+    
     export let artist: ArtistID3;
     export let api: SubsonicAPI;
     export let refreshViewOnClick: (event: MouseEvent) => void;
@@ -16,7 +15,7 @@
     <a href="/artists?id={artist.id}" on:click={refreshViewOnClick}>
         <div class="p-2 flex flex-col items-center w-40 h-auto">
 
-            <ImgCover api={api} title={artist.name} songId={artist.coverArt} />
+            <ImgCover size="32" marginRigth="0" disableModal={true} api={api} title={artist.name} songId={artist.coverArt} />
             
             <div class="h-full w-full text-center">
                 <span class="font-sans text-base font-medium leading-7 text-slate-900 dark:text-white line-clamp-2">{artist.name}</span>
--- src/components/app/search/partials/BoxMore.svelte
+++ src/components/app/search/partials/BoxMore.svelte
@@ -8,15 +8,15 @@
 </script>
 
 <div on:click={loadMoreOnClick} on:keypress={loadMoreOnClick} class="relative w-player flex shadow-player-light bg-player-light-background border-player-light-border dark:shadow-player-dark dark:bg-player-dark-background dark:border-player-dark-border transition-opacity select-none {loading ? 'opacity-20' : 'cursor-pointer'}">
-    <div class="p-2 flex flex-col items-center w-40 h-auto">
+    <div class="p-2 flex flex-col items-center justify-center w-40 h-auto">
 
         <div class="h-24 w-24 grid place-items-center">
             <PlusCircle class="w-20 h-20 rounded-sm border-bg-player-light-background dark:border-cover-dark-border"/>
         </div>
         
-        <div class="h-16 w-full text-center">
+        <!-- <div class="h-16 w-full text-center">
             <span class="font-sans text-base font-medium leading-7 text-slate-900 dark:text-white line-clamp-2">{title}</span>
-        </div>
+        </div> -->
 
     </div>
 </div>
