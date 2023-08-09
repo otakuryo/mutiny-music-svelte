e2643d305960aa80ab0bcd92f262d7e14c8ea88d
agustin <agustin.ryo@gmail.com>
2023-08-09 18:34:27 +0200

    style: player: Add border and spaces

--- src/components/app/player/PlaylistIndex.svelte
+++ src/components/app/player/PlaylistIndex.svelte
@@ -15,17 +15,23 @@
 </script>
 
 <div class="pl-3 main-right-panel">
-    <div class="main-color divide-y sticky top-0 z-10">
-        <PlayerInformationSong />
-        <PlayerControls />
-        <PlayerPlaylistInformation />
-    </div>
-    
-    <div class="h-100 divide-y">
-        {#each localPlaylist as song, index}
-            {#if !song.isDir}
-                <PlayerLineMusic bind:song={song} index={index} />
-            {/if}
-        {/each}
+    <div class="w-full flex flex-col max-h-screen-wo-menu">
+
+        <div class="main-color divide-y px-2 border-theme mx-2 mt-2">
+            <PlayerInformationSong />
+            <PlayerControls />
+            <PlayerPlaylistInformation />
+        </div>
+        
+        <div class="overflow-y-auto divide-y border-theme m-2">
+            
+            {#each localPlaylist as song, index}
+                {#if !song.isDir}
+                    <PlayerLineMusic bind:song={song} index={index} />
+                {/if}
+            {/each}
+
+        </div>
+
     </div>
 </div>
