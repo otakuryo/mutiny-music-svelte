9a49c8c8746a2d4dfda6e64768c8d1ab78950a34
agustin <agustin.ryo@gmail.com>
2023-09-11 18:24:56 +0200

    fix: themeBtn: On click and after esc press key, change theme

--- src/layouts/+ThemeToggle.svelte
+++ src/layouts/+ThemeToggle.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import { Moon, Sun } from "lucide-svelte";
     import { onMount } from 'svelte';
     
@@ -6,7 +6,7 @@
      * Toggle between dark and light theme and save the preference in localStorage
      */
     function changeThemeAppearance() {
-      
+
       let htmlClassList = document.documentElement.classList;
       if (htmlClassList.contains("dark")) {
         localStorage.setItem("theme", "light");
@@ -30,6 +30,12 @@
       }
     }
 
+    function onKeyupfn(event: KeyboardEvent) {
+      if (event.key === "Enter" || event.key === " " || event.key === "Spacebar" || event.key === "Space") {
+        changeThemeAppearance();
+      }
+    }
+
     /**
      * Set the theme appearance on page load
      */
@@ -38,7 +44,7 @@
     });
 </script>
 
-<div on:click={changeThemeAppearance} on:keyup={changeThemeAppearance} class="theme-toggle main-color-solid sm:top-4 bottom-4 right-4 ">
+<div on:click|preventDefault={changeThemeAppearance} on:keydown|preventDefault={onKeyupfn} class="theme-toggle main-color-solid sm:top-4 bottom-4 right-4 ">
   <button aria-label="theme toggle">
     <Moon class="w-6 dark:hidden" />
     <Sun class="w-6 dark:text-white dark:block hidden" />
